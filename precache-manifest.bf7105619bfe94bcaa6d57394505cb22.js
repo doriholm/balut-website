7ed8799527fae71626827aa85dc35825bc33b4be
Halldor Lyngmo <hly@umbraco.dk>
2020-07-15 00:26:28 +0000

    deploy

---
+++ precache-manifest.bf7105619bfe94bcaa6d57394505cb22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e10eb58681b3a92978",
+    "url": "/balut-website/css/app.80a1a99e.css"
+  },
+  {
+    "revision": "dfc4e7959a726576c743e56910793dcf",
+    "url": "/balut-website/index.html"
+  },
+  {
+    "revision": "53e10eb58681b3a92978",
+    "url": "/balut-website/js/app.95ba40ef.js"
+  },
+  {
+    "revision": "60b1f0ecc85f5154f11f",
+    "url": "/balut-website/js/chunk-vendors.245dde23.js"
+  },
+  {
+    "revision": "759a2032f0a66c680452d33cc6b382d6",
+    "url": "/balut-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/balut-website/robots.txt"
+  }
+]);
